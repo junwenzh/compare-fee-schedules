01064f82145569bf5723ebdd981b33527aae6d20
Jun <junwenzhang@outlook.com>
2023-10-11 16:07:32 -0400

    Added vite plugin to build a single html file

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import react from '@vitejs/plugin-react';
+import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vue(), viteSingleFile()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
