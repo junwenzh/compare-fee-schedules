b66f048e9cc291f82c4ddb5b65987704bd2e791d
Jun <junwenzhang@outlook.com>
2023-12-06 09:55:00 -0500

    Updated to csv function to handle dates in yyyy-mm-dd format

--- src/lib/mapToCsv/qnxtFormatToCsv.ts
+++ src/lib/mapToCsv/qnxtFormatToCsv.ts
@@ -1,15 +1,18 @@
 import { QnxtInputFormat, QnxtOutputFormat } from '@/types/QnxtFormat';
 
 export function qnxtInputToOutput(input: QnxtInputFormat): QnxtOutputFormat {
-  // const effective = `${input.effective.slice(0, 4)}-${input.effective.slice(
-  //   4,
-  //   6
-  // )}-${input.effective.slice(6, 8)}`;
-  const effectiveArr = input.effective.split('/');
-  const effective = `${effectiveArr[2].padStart(
-    2,
-    '0'
-  )}-${effectiveArr[0].padStart(2, '0')}-${effectiveArr[1]}`;
+  // if input.effective is not in format yyyy-mm-dd
+  // it is in mm/dd/yyyy
+  // create a new string in format yyyy-mm-dd
+  let effective = input.effective;
+
+  if (!effective.match(/\d{4}-\d{2}-\d{2}/)) {
+    const effectiveArr = effective.split('/');
+    effective = `${effectiveArr[2].padStart(4, '0')}-${effectiveArr[0].padStart(
+      2,
+      '0'
+    )}-${effectiveArr[1].padStart(2, '0')}`;
+  }
 
   // split the modifer into two parts
   const mod = input.mod.match(/.{1,2}/g);
@@ -39,7 +42,7 @@ export function qnxtInputToOutput(input: QnxtInputFormat): QnxtOutputFormat {
 function qnxtOutputToCsv(array: QnxtOutputFormat[]) {
   // join each line with \n
   const lines = array.map(line => line.join(','));
-  // console.log(lines);
+  console.log(lines);
   // join each cell with ,
   const csv = lines.join('\n');
   // add the header
@@ -70,7 +73,7 @@ export function inputMapToCsv(map: Map<string, QnxtInputFormat>): string {
     try {
       output.push(qnxtInputToOutput(v));
     } catch (e) {
-      console.log('error', v);
+      console.log('error in qnxtFormatToCsv.ts, inputMapToCsv', v, e);
     }
   });
   const csv = qnxtOutputToCsv(output);
